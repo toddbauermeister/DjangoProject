193674c6c786a896253a4f0458dd06b52eb49036
Todd Bauermeister <toddbauermeister@gmail.com>
2016-10-01 22:39:24 +0200

    Added __str__ methods and added surname field to Driver

--- packages/models.py
+++ packages/models.py
@@ -2,16 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-class Client(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name + " " + self.surname
-
-
 class Branch(models.Model):
     name = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
@@ -24,11 +14,12 @@ class Package(models.Model):
     reference_number = models.CharField(max_length=10)
     status = models.CharField(max_length=50)
     volumetric_weight = models.DecimalField(max_digits=10, decimal_places=2)
-    destination = models.CharField(max_length=50)
-    destination_city = models.CharField(max_length=50)
+    client_address = models.CharField(max_length=50)
+    client_city = models.CharField(max_length=50)
+    receiver_address = models.CharField(max_length=50)
+    receiver_city = models.CharField(max_length=50)
     id_required = models.BooleanField(default=True)
-    client = models.ForeignKey(Client, default=1)
-    driver = models.ForeignKey(Client, default=None)
+    user = models.ForeignKey(User, default=1)
 
     def get_statuses(self):
         statuses = [
@@ -42,6 +33,8 @@ class Package(models.Model):
             'Lost',
             'Damaged'
         ]
+
+        return statuses
 
     def __str__(self):
         return "Package " + self.reference_number
