dbce308ba1602ddbe770364a5e697af8a7548447
Brendan <drummer.bren@gmail.com>
2016-10-01 22:47:31 +0200

    Added bootstrap3 to settings and created requirements text.
    Bootstrap skeleton is in packages/templates

--- packages/views.py
+++ packages/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 
 def mypacks(request):
-    return HttpResponse("Hello, world. You're at the package index.")
+    return render(request, 'base_template.html')
+
