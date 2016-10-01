84f43fddf853143ef996cd58618e835ca8d759c3
Todd Bauermeister <toddbauermeister@gmail.com>
2016-10-01 21:56:00 +0200

    Added __str__ methods and added surname field to Driver

--- packages/models.py
+++ packages/models.py
@@ -22,20 +22,16 @@ class Branch(models.Model):
 
 class Package(models.Model):
     reference_number = models.CharField(max_length=10)
-    receiver_name = models.CharField(max_length=50)
-    receiver_surname = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
     volumetric_weight = models.DecimalField(max_digits=10, decimal_places=2)
     destination = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
     id_required = models.BooleanField(default=True)
     client = models.ForeignKey(Client, default=1)
+    driver = models.ForeignKey(Client, default=None)
 
     def __str__(self):
         return "Package " + self.reference_number
-
-
-class WarehouseManager(models.Model):
-    branch = models.OneToOneField(Branch, default=1)
 
 
 class Driver(models.Model):
