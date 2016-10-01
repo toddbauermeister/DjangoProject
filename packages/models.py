87a67264cde13b54ac5241bb49361ee0fd248a29
Todd Bauermeister <toddbauermeister@gmail.com>
2016-10-02 00:51:33 +0200

    Merge branch 'master' of /home/todd/DjangoProject with conflicts.

--- packages/models.py
+++ packages/models.py
@@ -1,18 +1,46 @@
-from django.contrib.auth.models import Permission, User
 from django.db import models
+from django.utils import timezone
 
 
 class Branch(models.Model):
     name = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
 
+    def get_branch_offices(self):
+
+        satellite_offices = [
+            'Swellendam',
+            'Observatory',
+            'Kloof',
+            'Hillcrest'
+            'Hatfield',
+            'Sunnyside',
+            'Parktown',
+            'Sandton'
+        ]
+
+        return satellite_offices
+
+    def get_satellite_offices(self):
+
+        satellite_offices = [
+            'Cape Town',
+            'Durban',
+            'Pretoria',
+            'Johannesburg'
+        ]
+
+        return satellite_offices
+
+    def get_package_location_prefix(self):
+
+        prefixes = ['Arrived At','Left']
+
     def __str__(self):
         return self.name + " - " + self.city
 
 
 class Package(models.Model):
-
-    user = models.ForeignKey(User, default=1)
     reference_number = models.CharField(max_length=10)
     status = models.CharField(max_length=50)
     volumetric_weight = models.DecimalField(max_digits=10, decimal_places=2)
@@ -21,7 +49,8 @@ class Package(models.Model):
     receiver_address = models.CharField(max_length=50)
     receiver_city = models.CharField(max_length=50)
     id_required = models.BooleanField(default=True)
-
+    user = models.ForeignKey(User, default=1)
+    branch = models.ForeignKey(Branch, default=1)
 
     def get_statuses(self):
         statuses = [
@@ -50,7 +79,4 @@ class Driver(models.Model):
 
     def __str__(self):
         return self.name + " " + self.surname
-
-
-
 
--- packages/views.py
+++ packages/views.py
@@ -1,25 +1,86 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
-from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from .forms import PackageForm, UserForm
-from .models import User, Branch, Package, WarehouseManager, Driver
+from models import Package
+from models import Driver
+from models import Branch
 
 
-
-def mypacks(request):
-    return HttpResponse("Hello, world. You're at the package index.")
-
-def create_package(request):
-    if not request.client.is_authenticated():
+def update_package_status(request):
+    if not request.user.is_authenticated():
         return render(request, 'packages/login.html')
+
     else:
-        form = PackageForm(request.POST or None, request.FILES or None)
-        if form.is_valid():
-            package = form.save(commit=False)
-            package.user = request.client
+        packages = Package.objects.filter(user=request.user)
+        drivers = Driver.objects.all()
+        statuses = Package.get_statuses() #In template -> Check If Package is in office. If it is, let user pick In and out of branch and satellite offices
+        satellite_offices = Branch.get_satellite_offices()
+        branch_offices = Branch.get_branch_offices()
+
+        context = {'packages': packages, 'statuses': statuses, 'satellite_offices': satellite_offices}
+
+        return request, 'packages/update_package', context
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
