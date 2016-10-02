9b9b6bbeaefec765a501d621b8f6f30483b87f1f
Werner Kotze <blink182wernerk@gmail.com>
2016-10-02 18:03:48 +0200

    1) Made changes to login

--- MuLogistics/urls.py
+++ MuLogistics/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
-    url(r'', include('packages.urls')),
+    url(r'^packages/', include('packages.urls')),
+    url(r'^', include('packages.urls')),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
