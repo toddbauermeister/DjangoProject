e66aea6b6534b9c44c23ce732c3c1d9798616504
Werner Kotze <blink182wernerk@gmail.com>
2016-10-02 02:21:31 +0200

    Changed a lot

--- MuLogistics/urls.py
+++ MuLogistics/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^packages/', include('packages.urls')),
+    url(r'^packages/', include('package.urls')),
 ]
